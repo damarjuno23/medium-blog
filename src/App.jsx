adeeed58537e03836b967f381c56ac8ef84e0f0c
jono <jojonmojo35@gmail.com>
2025-12-24 15:54:51 +0700

    header is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home/Home";
+import Demo from "./Demo/Demo";
+import DemoHeader from "./Demo/DemoHeader";
+import HomeHeader from "./Home/HomeHeader";
 
 export default function App() {
+  const auth = false;
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    {auth ? <HomeHeader /> : <DemoHeader />}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/demo" element={<Demo />} />
+    </Routes>
+    </>
   )
 }
